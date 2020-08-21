784e727d8ae9a4651eaa0b3f4456285c2b195d5f
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-08-21 10:05:18 -0700

    seed database context

--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -10,5 +10,17 @@ namespace AnimalShelter.Models
         }
 
         public DbSet<Animal> Animals { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+          builder.Entity<Animal>()
+          .HasData(
+            new Animal { AnimalId = 1, Name = "Matilda", Type = "Cat", Age = 7, Gender = "Female" },
+            new Animal { AnimalId = 2, Name = "Rexie", Type = "Dog", Age = 10, Gender = "Female" },
+            new Animal { AnimalId = 3, Name = "Matilda", Type = "Dog", Age = 2, Gender = "Female" },
+            new Animal { AnimalId = 4, Name = "Pip", Type = "Cat", Age = 4, Gender = "Male" },
+            new Animal { AnimalId = 5, Name = "Bartholomew", Type = "Dog", Age = 5, Gender = "Male" }
+          );
+        }
     }
 }
