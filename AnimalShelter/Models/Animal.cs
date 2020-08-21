e53b441574354cb5691528d1156b6c54a1e110b3
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-08-21 15:49:58 -0700

    fix listening port

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -1,19 +1,11 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace AnimalShelter.Models
+  namespace AnimalShelter.Models
 {
     public class Animal
     {
         public int AnimalId { get; set; }
-        [Required]
-        [StringLength(20)]
         public string Name { get; set; }
-        [Required]
         public string Type { get; set; }
-        [Required]
-        [Range(0, 50, ErrorMessage = "Age must be between 0 and 50.")]
         public int Age { get; set; }
-        [Required]
         public string Gender { get; set; }
     }
 }
