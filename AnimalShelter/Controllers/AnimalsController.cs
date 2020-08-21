8b492b971a823a7982874e37231d48c1a28ee9c6
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-08-21 09:01:26 -0700

    add animals controller

---
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using AnimalShelter.Models;
+
+namespace AnimalShelter.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AnimalsController : ControllerBase
+  {
+    private AnimalShelterContext _db;
+
+    public AnimalsController(AnimalShelterContext db)
+    {
+      _db = db;
+    }
+  }
+}
